4c6512cdb0347ca5c94018b9712498325b2a3517
h2oalphaYT <chanukaddevin@gmail.com>
2023-08-30 19:02:24 +0530

    Clodinary setup

---
+++ BACKEND/middlewares/UploadImages.js
@@ -0,0 +1,47 @@
+const multer = require("multer")
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/images/"));
+  },
+  filename: function (req, file, cb) {
+    const uniquesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniquesuffix + ".jpeg");
+  },
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb({ message: "Unsupported file format" }, false);
+  }
+};
+
+const uploadPhoto = multer({
+  storage: storage,
+  fileFilter: multerFilter,
+  limits: { fileSize: 2000000 },
+});
+
+// const SupplierProfileImgResize = async (req, res, next) => {
+//   if (!req.files) return next();
+//   await Promise.all(
+//     req.files.map(async (file) => {
+//       await sharp(file.path)
+//         .resize(300, 300)
+//         .toFormat("jpeg")
+//         .jpeg({ quality: 90 })
+//         .toFile(`public/images/SupplierProfile/${file.filename}`);
+//       fs.unlinkSync(`public/images/SupplierProfile/${file.filename}`);
+//     })
+//   );
+//   next();
+// };
+
+
+
+module.exports = { uploadPhoto };
--- BACKEND/routes/SupplierRoutes.js
+++ BACKEND/routes/SupplierRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const supplierController = require("../controller/supplierController");
 
 
+
 router.route("/addSupplier").post(supplierController.addSupplier);
 router.route("/").get(supplierController.getAllSuppliers);
 router.route("/update/:id").put(supplierController.updateSupplier);
 router.route("/delete/:id").delete(supplierController.deleteSupplier);
 router.route("/get/:id").get(supplierController.getSupplierById);
+
 
 
 module.exports = router;
