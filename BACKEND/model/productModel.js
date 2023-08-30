7e8464cc40636bed3017428d114a5acda8f7ebd3
Osadha Ganewaththa <110708704+IT21802058@users.noreply.github.com>
2023-08-30 17:26:02 +0530

    Product model, controller, and routes are added

---
+++ BACKEND/model/productModel.js
@@ -0,0 +1,71 @@
+//require the mongoose to the model
+const mongoose = require('mongoose');
+
+//declare the schema of the product model
+var productSchema = new mongoose.Schema({
+    
+    productID:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    Title:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    slug:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+
+    description:{
+        type: String,
+        required: true,
+    },
+
+    price:{
+        type: Number,
+        required: true,
+    },
+
+    discount:{
+        type: Number,
+        required: true,
+    },
+
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+    },
+
+    brand:{
+        type: String,
+        enum: ["Bajaj", "TVS", "Boxer", "Hero"]
+    },
+
+    images:{
+        type: Array,
+    },
+
+    color:{
+        type: String,
+        enum: ["Black", "Red", "Blue", "Yellow"]
+    },
+
+    ratings:{
+        star: Number,
+        postedby: {type: mongoose.Schema.Types.ObjectId, 
+            ref: "User"}
+    },
+},
+{timestamps: true}
+);
+
+const Product = mongoose.model("Product" , productSchema);
+
+module.exports = Product;
