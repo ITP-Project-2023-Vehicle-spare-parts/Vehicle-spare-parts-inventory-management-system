6bb44381ede4a669679f44b5e59ae77ff404c8d2
h2oalphaYT <chanukaddevin@gmail.com>
2023-08-31 21:37:23 +0530

    Front end Supplier Navgation and folder structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
-
+import {BrowserRouter as  Router ,Route, Routes} from "react-router-dom"
 import './App.css';
+import SupplierSideNavigation from "./components/SupplierSideNavigation";
+import AddSupplier from "./components/AddSupplier"
+import AddClient from "./components/AddClient";
 
 function App() {
   return (
-   
+   <Router>
+    <div>
+
+    <SupplierSideNavigation/>
+
+    <Routes>
+      <Route path ="/Admin/sup/add" element={<AddSupplier/>} />
+      <Route path ="/Admin/client/add" element={<AddClient/>} />
+    </Routes>
+
+
+
+    </div>
+   </Router>
       
-       <center><h1>Veda krapalla</h1></center> 
+      
      
    
   );
---
+++ frontend/src/components/SupplierSideNavigation.js
@@ -0,0 +1,153 @@
+// import React , {useState ,useEffect} from "react";
+// import axios from "axios";
+import React,{useState} from "react";
+import { Link } from "react-router-dom";
+import 'boxicons/css/boxicons.min.css';
+import '../CSS/SupplierSideNavigation.css';
+
+
+
+export default function SupplierSideNavigation() {
+
+    // let btn = document.querySelector("#btn");
+    // let sidebar = document.querySelector(".sidebar");
+    
+
+    //  btn.onclick = function(){
+    //     sidebar.classList.toggle('active');
+
+    //  } 
+
+    const [sidebarActive, setSidebarActive] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebarActive(!sidebarActive);
+    };
+     
+   
+    return(
+     <div>
+
+<div className={`sidebar ${sidebarActive ? "active" : ""}`}>
+
+        <div className="sidebar">
+            <div className="logo_content">
+                <div className="logo">
+                    <div className="logo_name">
+                    
+                        <img className="component-3-icon" alt="" src="images/CMLogo.png"/>
+                        <div className="topic-text"><span style={{color:"blue"}}>CM</span> Spare</div>
+                       
+                        
+                        
+                    </div>
+                    <i className="bx bx-menu" id="btn" onClick={toggleSidebar}></i> 
+                </div>
+
+                
+            </div>
+
+            <ul className="nav_list">
+                <li>
+                
+                    < i className='bx bx-search' onClick={toggleSidebar}></i>
+                   <input type="text" placeholder="Search..."/> 
+               
+                    <dev className="tooltip">Search</dev>
+
+                </li>
+                <li>
+                <Link to="/">
+                    < i className='bx bx-grid-alt'></i>
+                    <span className="link-name">Dashboard</span>
+                    </Link>
+                    <dev className="tooltip">Dashboard</dev>
+
+                </li>
+                <li>
+                <Link to="/">
+                <i class='bx bx-user' ></i>
+                    <span className="link-name">User</span>
+                    </Link>
+                    <dev className="tooltip">User</dev>
+
+                </li>
+               
+                <li>
+                <Link to="/">
+                <i className='bx bx-stats'></i>
+                    <span className="link-name">Analytics</span>
+                    </Link>
+                    <dev className="tooltip">Analytics</dev>
+
+                </li>
+                <li>
+                <Link to="/">
+                <i className='bx bxs-package' ></i>
+                    <span className="link-name">Prodects</span>
+                    </Link>
+                    <dev className="tooltip">Prodects</dev>
+
+                </li>
+                <li>
+                <Link to="/">
+                <i class='bx bx-money' ></i>
+                    <span className="link-name">Payment</span>
+                    </Link>
+                    <dev className="tooltip">Payment</dev>
+
+                </li>
+                <li>
+                <Link to="/">
+                <i class='bx bx-cart-alt'></i>
+                    <span className="link-name">Orders</span>
+                    </Link>
+                    <dev className="tooltip">Orders</dev>
+
+                </li>
+                <li>
+                <Link to="/">
+                <i class='bx bxs-bell' ></i>
+                    <span className="link-name">Notification</span>
+                    </Link>
+                    <dev className="tooltip">Notification</dev>
+
+                </li>
+               
+
+
+            </ul>
+
+            <div className="profile_content">
+                <div className="profile">
+                   <div className="profile_details">
+                    <img src="images/me.jpg" alt=""></img>
+                    <div className="name_job">
+                        <div className="name">Chanuka Devin</div>
+                        <div className=""><span style={{color:"#7C7C7C"}}> YAMAHA Company</span></div>
+                    </div>
+                   </div>
+                   <i className="bx bx-log-out" id="log_out"></i>
+
+
+                </div>
+            </div>
+        </div>
+
+        {/* <div className="home_content">
+            <div className="text">Supplier Dashboard</div>
+        </div> */}
+
+        </div>
+
+
+
+
+
+
+  </div>
+
+  
+    )
+}
+  
