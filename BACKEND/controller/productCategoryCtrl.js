004fbb7daaff0c8ee6fb27254c57e07be3ddae6a
Osadha Ganewaththa <110708704+IT21802058@users.noreply.github.com>
2023-09-06 14:21:52 +0530

    updated product category

--- BACKEND/controller/productCategoryCtrl.js
+++ BACKEND/controller/productCategoryCtrl.js
@@ -11,4 +11,49 @@ const createCategory = asyncHandler(async (req, res)=>{
     }
 });
 
-module.exports = {createCategory};
+const updateCategory = asyncHandler(async (req, res)=>{
+    const id = req.params.id;
+    validateMongoDbId(id);
+    try{
+        const updatedCategory =  await Category.findByIdAndUpdate(id, req.body,{
+            new: true,
+        });
+        res.json(updatedCategory);
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+const deleteCategory = asyncHandler(async (req, res)=>{
+    const id = req.params.id;
+    validateMongoDbId(id);
+    try{
+        const deletedCategory =  await Category.findByIdAndDelete(id);
+        res.json(deletedCategory);
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+const getACategory = asyncHandler(async (req, res)=>{
+    const id = req.params.id;
+    validateMongoDbId(id);
+    try{
+        const getCategory =  await Category.findById(id);
+        res.json(getCategory);
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+const getAllCategory = asyncHandler(async (req, res)=>{
+    try{
+        const getallCategory =  await Category.find();
+        res.json(getallCategory);
+    }catch(error){
+        throw new Error(error);
+    }
+});
+
+
+module.exports = {createCategory, updateCategory, deleteCategory, getACategory, getAllCategory};
--- BACKEND/routes/productCategoryRoute.js
+++ BACKEND/routes/productCategoryRoute.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { createCategory } = require("../controller/productCategoryCtrl");
+const { createCategory, updateCategory, deleteCategory, getACategory, getAllCategory } = require("../controller/productCategoryCtrl");
 const router = express.Router();
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 
 router.post("/addCategory/",authMiddleware, isAdmin,createCategory);
+router.put("/updateCategoryProduct/:id",authMiddleware, isAdmin,updateCategory);
+router.delete("/deletCategoryProduct/:id",authMiddleware, isAdmin, deleteCategory);
+router.get("/getaCategoryProduct/:id",authMiddleware, isAdmin, getACategory);
+router.get("/getAllCategoryProduct", getAllCategory);
+
 module.exports = router;
