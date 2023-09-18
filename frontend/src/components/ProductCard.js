bd73ff58047a2a0ae7417205079904a604004087
Osadha Ganewaththa <110708704+IT21802058@users.noreply.github.com>
2023-09-19 04:50:35 +0530

    store pages added

---
+++ frontend/src/components/ProductCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactStars from 'react-rating-stars-component';
+import {Link, useLocation} from 'react-router-dom'
+
+const ProductCard = (props) => {
+    const {grid} = props;
+    let location = useLocation();
+    
+  return (
+    <div className={`${location.pathname === "/store" ? `gr-${grid}` : "col-3"}`}>
+        <div className='product-card position-relative'>
+            <div className='wishlist-icon position-absolute'>
+                <Link>
+                    <img src='images/wish.svg' alt='wishlist' />
+                </Link>
+            </div>
+            <div className='product-image'>
+                <img src='images/oilcan.jpg' className='img-fluid' alt="product pic" />
+                <img src='images/spare 5.1.jpg' className='img-fluid' alt="product pic" />
+            </div>
+            <div className='product-details'>
+                <h6 className='brand'>Bajaj</h6>
+                <h5 className='product-title'>
+                    Engine oils for CT100.
+                </h5>
+                <ReactStars count={5} size={24} value='3' edit={false} activeColor={"#ffd700"} />
+                <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+                Motor oil, engine oil, or engine lubricant is any one of various substances used for the lubrication of internal combustion engines. They typically consist of base oils enhanced with various additives, particularly antiwear additives, detergents, dispersants, and, for multi-grade oils, viscosity index improvers
+                </p>
+                <p className='price'>Rs.750.00</p>
+            </div>
+            <div className='action-bar position-absolute'>
+                <div className='d-flex flex-column gap-15'>
+                    <Link>
+                        <img src='images/view.svg' alt='view' />
+                    </Link>
+                    <Link>
+                        <img src='images/add-cart.svg' alt='addcart' />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- frontend/src/pages/store.js
+++ frontend/src/pages/store.js
@@ -1,16 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Helmet from 'react-helmet';
+import ProductCard from '../components/ProductCard';
 
-const store = () => {
+const Store = () => {
+  const [grid, setGrid] = useState(4);
+  alert(grid);
   return (
     <>
     <Helmet>
       <meta charSet="utf-8" />
       <title>Store</title>
     </Helmet>
-    <p>hi cudda</p>
+    <div className='home-wrapper-2'>
+      <center><h4><br/>.....STORE.....<br/><br/></h4></center>
+    </div>
+
+    <div className='store-wrapper home-wrapper-2 py-5'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-3'>
+            <div className='filter-card mb-3'>
+              <h3 className='filter-title'>Shop By Category</h3>
+              <div>
+                <ul className='ps-0'>
+                  <li>BAJAJ</li>
+                  <li>HERO</li>
+                  <li>YAMAHA</li>
+                  <li>PLATINA</li>
+                </ul>
+              </div>
+            </div>
+            <div className='filter-card mb-3'>
+              <h3 className='filter-title'>Filter By</h3>
+              <div>
+                <h5 className='sub-title'>Price (Rs.)</h5>
+                <div className='d-flex align-items-center gap-10'>
+                <div className="form-floating">
+                  <input type="email" className="form-control" id="floatingInput" placeholder="From" />
+                  <label htmlFor="floatingInput">From</label>
+                </div>
+                <div className="form-floating">
+                  <input type="email" className="form-control" id="floatingInput1" placeholder="To"/>
+                  <label htmlFor="floatingInput1">To</label>
+                </div>
+                </div>
+                <h5 className='sub-title'>Colors</h5>
+                <div>
+                    <ul className='colors ps-0'>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                </div>
+                
+              </div>
+            </div>
+            <div className='filter-card mb-3'>
+              <h3 className='filter-title'>Product tag</h3>
+              <div>
+                <div className='product-tags d-flex flex-wrap align-items-center gap-10'>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    CT100
+                  </span>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    NS150
+                  </span>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    NS200
+                  </span>
+                  <span className='badge bg-light text-secondary rounded-3 py-2 px-3'>
+                    ZR100
+                  </span>
+                </div> 
+              </div>
+            </div>
+          </div>
+          <div className='col-9'>
+            <div className='filter-sort-grid mb-4'>
+              <div className='d-flex justify-content-between align-items-center gap-10'>
+              <div className='d-flex align-items-center '>
+                <p className='mb-0 d-block'>Sort_by: </p>
+                <select name='' className='form-control form-select sort_style_backgrond' id=''>
+                    <option value="manual">Featured</option>
+                    <option value='best-selling' selected="selected">Best selling</option>
+                    <option value='title-ascending' selected="selected">Alphabetically: A - Z</option>
+                    <option value='title-descending' selected="selected">Alphabetically: Z - A</option>
+                    <option value='price-ascending' selected="selected">Price: low to high</option>
+                    <option value='price-descending' selected="selected">Price: high to low</option>
+                </select>
+              </div>
+              <div className='d-flex align-items-center gap-10'>
+                <p className='totalproducts'>Products</p>
+                <div className='d-flex gap-10 align-items-center grid'>
+                  <img onClick={()=>{setGrid(3);}} src='images/gr4.svg' className='d-block img-fluid' alt='grid' />
+                  <img onClick={()=>{setGrid(4);}} src='images/gr3.svg' className='d-block img-fluid' alt='grid' />
+                  <img onClick={()=>{setGrid(6);}} src='images/gr2.svg' className='d-block img-fluid' alt='grid' />
+                  <img onClick={()=>{setGrid(12);}} src='images/gr.svg' className='d-block img-fluid' alt='grid' />
+                </div>
+              </div>
+              </div>
+            </div>
+            <div className='product-list pb-5'>
+              <div className='d-flex gap-10 flex-wrap'>
+              <ProductCard grid={grid} />
+              <ProductCard grid={grid} />
+              <ProductCard grid={grid} />
+              <ProductCard grid={grid} />
+              <ProductCard grid={grid} />
+              <ProductCard grid={grid} />
+              <ProductCard grid={grid} />
+              <ProductCard grid={grid} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   );
 };
 
-export default store;
+export default Store;
