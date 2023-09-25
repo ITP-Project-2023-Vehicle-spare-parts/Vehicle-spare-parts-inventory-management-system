db98c9cc63c4ac9ccc6b5a087148a179ad37fed6
h2oalphaYT <chanukaddevin@gmail.com>
2023-09-25 16:52:21 +0530

    app.js modyfied

--- frontend/src/components/MainLayout.js
+++ frontend/src/components/MainLayout.js
@@ -90,6 +90,7 @@ const MainLayout = () => {
               key: 'branch',
               icon: <AiOutlineBranches className='fs-4'/>,
               label: 'Branch',
+              
             },
             {
               key: 'delivery',
