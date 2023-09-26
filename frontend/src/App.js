92ebf010e8fc2ad6483f472c07f6861737be9906
h2oalphaYT <chanukaddevin@gmail.com>
2023-09-26 22:58:21 +0530

    update profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,6 +35,7 @@ import CategoryBarGraph from "./components/stockComponents/barGraph";
 import LowStock from "./components/stockComponents/lowStock";
 import UpdateStock from "./components/stockComponents/updateStock";
 
+
 import AddClaim from './components/ClaimComponents/AddClaim';
 import FetchClaim from './components/ClaimComponents/FetchClaim';
 import UpdateClaim from './components/ClaimComponents/UpdateClaim';
@@ -42,7 +43,10 @@ import DeleteClaim from './components/ClaimComponents/DeleteClaim';
 import AllClaims from './components/AdminClaimComponent/AllClaims';
 import AdminUpdateclaim from './components/AdminClaimComponent/AdminUpdateclaim';
 
+
+
 import ClientProfile from "./components/Client/ClientProfile";
+import ClientProfileUpdate from "./components/Client/ClientProfileUpdate";
 
 function App() {
   return (
@@ -68,6 +72,7 @@ function App() {
             <Route path="/Admin/profile/update/:id" element={<UpdateProfileAdmin />} />
             <Route path="/Admin/Sup/Profile/:id" element={<SupplierProfile />} />
             <Route path="/Admin/client/Profile/:id" element={<ClientProfile />} />
+            <Route path="/Admin/client/profile/update/:id" element={<ClientProfileUpdate />} />
 
             <Route path="/admin/piechart" element={<CategoryPieChart />} />
             <Route path="/admin/bargraph" element={< CategoryBarGraph/>} />
@@ -76,6 +81,8 @@ function App() {
 
             <Route path="/" element={<AllClaims />} />
             <Route path="admin/updates/:id/:billno" element={<AdminUpdateclaim/>} />
+
+         
 
             {/* Claim Part */}
               <Route path="/addclaim" element={<AddClaim />} />
