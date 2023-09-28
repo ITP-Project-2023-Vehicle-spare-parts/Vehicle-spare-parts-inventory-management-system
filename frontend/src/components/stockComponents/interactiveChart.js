a24e2789e7e2f5254dcd7906cbee33d4b278aca0
Hashini Rathnayake <110319538+IT21802508@users.noreply.github.com>
2023-09-28 16:38:25 +0530

    Updated Stock Frontend

--- frontend/src/components/stockComponents/interactiveChart.js
+++ frontend/src/components/stockComponents/interactiveChart.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import CategoryBarGraph from './barGraph'; // Assuming you have this component
-
+//import CategoryBarGraph from './barGraph'; // Assuming you have this component
 import LowStockBarGraph from './lowStockGraph';
 
-const chartContainerStyle = {
-  marginBottom: '20px',
-  padding: '20px',
-  border: '1px solid #ccc',
-  borderRadius: '5px',
-  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-  backgroundColor: '#fff',
-};
+
 
 
 
@@ -19,15 +11,11 @@ const InteractiveChartsPage = () => {
   return (
     <Container>
       <Row>
-        <Col lg={6}>
-          <div style={chartContainerStyle}>
-            <CategoryBarGraph />
-          </div>
-        </Col>
-        <Col lg={6}>
-          <div style={chartContainerStyle}>
+          
+        <Col lg={6} style={{ marginBottom: '20px',  marginLeft: '180px' }}>
+      
             <LowStockBarGraph />
-          </div>
+         
         </Col>
       </Row>
     </Container>
