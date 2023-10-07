a74c43598527e9354f3ddfdad77da32b3e2ebf94
Osadha Ganewaththa <110708704+IT21802058@users.noreply.github.com>
2023-10-08 01:07:05 +0530

    store updated

--- frontend/src/pages/store.js
+++ frontend/src/pages/store.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ProductCard from '../components/ProductCard';
-import Color from '../components/Color';
+//import Color from '../components/Color';
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import {useDispatch, useSelector} from "react-redux";
