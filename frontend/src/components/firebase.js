88a3ea5d57ecee93283c610660c513c8fb8833ad
it21827662 <sathiradissanayaka45@gmail.com>
2023-10-17 04:21:00 +0530

    remove unwanted things

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBmX_0IFHmalTw0BjKu2_gML8fje1gjgM",
   authDomain: "spare-parts-delivery.firebaseapp.com",
@@ -14,7 +11,6 @@ const firebaseConfig = {
   appId: "1:541692476776:web:7fe4665568e8dda689aadd"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
